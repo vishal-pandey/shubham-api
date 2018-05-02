58478560e18603e7f1e2aa9f9b08bd441e9557c7
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-02 06:23:27 +0530

    script upload

--- user/getuserstatus.php
+++ user/getuserstatus.php
@@ -1,38 +1,35 @@
 <?php
 	include "../db/connect.php";
-	echo $_POST['userid'];
-	// $sql = " select count(pid) as total from task where pid in (select pid from assignment where userid = '{$userid}')";
+	$userid = $_POST['userid'];
+	$sql = " select count(pid) as total from task where pid in (select pid from assignment where userid = '{$userid}')";
 
-	// $result = $conn->query($sql);
-	// $obj = new StdClass();
-	// $obj = $result->fetch_object();
+	$result = $conn->query($sql);
+	$obj = new StdClass();
+	$obj = $result->fetch_object();
 
-	// echo $total = $obj->total;
-	// echo $sql;
+	$total = $obj->total;
+
+	$sql = " select count(pid) as complete from task where pid in (select pid from assignment where userid = '{$userid}') and progress = '1'";
+
+	$result = $conn->query($sql);
+	$obj = new StdClass();
+	$obj = $result->fetch_object();
+
+	$complete = $obj->complete;
 
 
-	// $sql = " select count(pid) as complete from task where pid in (select pid from assignment where userid = '{$userid}') and progress = '1'";
+	$sql = " select count(pid) as uncomplete from task where pid in (select pid from assignment where userid = '{$userid}') and progress = '0'";
 
-	// $result = $conn->query($sql);
-	// $obj = new StdClass();
-	// $obj = $result->fetch_object();
+	$result = $conn->query($sql);
+	$obj = new StdClass();
+	$obj = $result->fetch_object();
 
-	// $complete = $obj->complete;
-	// echo $sql;
-
-	// $sql = " select count(pid) as uncomplete from task where pid in (select pid from assignment where userid = '{$userid}') and progress = '0'";
-
-	// $result = $conn->query($sql);
-	// $obj = new StdClass();
-	// $obj = $result->fetch_object();
-
-	// $uncomplete = $obj->uncomplete;
-	// echo $sql;
+	$uncomplete = $obj->uncomplete;
 	
-	// echo '{
-	// 				"total": "'.$total.'",
-	// 				"complete": "'.$complete.'",
-	// 				"uncomplete": "'.$uncomplete.'"
-	// 			}
-	// 				';
+	echo '{
+					"total": "'.$total.'",
+					"complete": "'.$complete.'",
+					"uncomplete": "'.$uncomplete.'"
+				}
+					';
 ?>
