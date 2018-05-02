ed594f3653c813d12aeac9cd435cbe0442744446
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-02 06:09:44 +0530

    script upload

--- user/getuserstatus.php
+++ user/getuserstatus.php
@@ -1,6 +1,6 @@
 <?php
 	include "../db/connect.php";
-	$userid = $_POST['userid'];
+	echo $userid = $_POST['userid'];
 	$sql = " select count(pid) as total from task where pid in (select pid from assignment where userid = '{$userid}')";
 
 	$result = $conn->query($sql);
