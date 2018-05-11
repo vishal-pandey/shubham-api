bbc1e38677ac43b80bd3992bf3efc47923c5e669
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-11 06:31:57 +0530

    script upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+	include "../db/connect.php";
+
+	$sql = "select * from switch where sn = '1'";
+
+
+	$result = $conn->query($sql);
+	$obj = new StdClass();
+	
+	$obj = $result->fetch_object();
+
+	echo json_encode($obj);
+
+?>
